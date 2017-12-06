47776a02d52e9d0f39d6f0a3ef2b95c02d0f49c2
root <root@cantelope.net>
2017-12-05 23:24:49 -0800

    tweaked levels

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (()=>{
 
   var score = 0,
-  level=1,
+  1,
   highScores=[],bullets = [],splosions = [],
   bubbles = [],enemies = [],bumps=[],powerups=[],coins=[],LHS=[],
   spritesheet,gameOverPal,enemyPal,last = 0,sides,
@@ -686,7 +686,7 @@
     }
     if(spacekey){
       spokes=3;
-      level=7;
+      level=1;
       startup()
     }
   }
