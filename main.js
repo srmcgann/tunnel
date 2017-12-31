5304676daf11e6b58dcb93db1c95c8ea865085ea
root <root@cantelope.net>
2017-12-30 19:46:00 -0800

    fixed invincibility bug

--- main.js
+++ main.js
@@ -908,7 +908,7 @@
             while(p>Math.PI)p-=Math.PI*2
             while(p<-Math.PI)p+=Math.PI*2
             //check for squeeze to prevent killing all at once from sideways movement
-            if(squeeze > .98 || squeeze < .02){
+            //if(squeeze > .98 || squeeze < .02){
               if(Math.abs(e.theta - p) < 0.2 ){
                 X=S(s*2*j*playerZ+d)*3/FOV*300-f,Y=C(s*3*j*playerZ+t/(1000/vert))*.5/FOV*300-g;
                 X+=S(p = squeeze < .02 ? playerTheta : playerTheta+Math.PI*2/spokes*((i+.5)-spokes/2)*squeeze),Y+=C(p);
@@ -921,7 +921,7 @@
                 eArr.splice(eIndex, 1);
                 break;
               }
-            }
+            //}
           }
         }
       }
@@ -945,7 +945,7 @@
             while(p>Math.PI)p-=Math.PI*2
             while(p<-Math.PI)p+=Math.PI*2
             //check for squeeze to prevent killing all at once from sideways movement
-            if(squeeze > .98 || squeeze < .02){
+            //if(squeeze > .98 || squeeze < .02){
               if(Math.abs(e.theta - p) < .2 ){
                 X=S(s*2*j*playerZ+d)*3/FOV*300-f,Y=C(s*3*j*playerZ+t/(1000/vert))*.5/FOV*300-g;
                 X+=S(p = squeeze < .02 ? playerTheta : playerTheta+Math.PI*2/spokes*((i+.5)-spokes/2)*squeeze),Y+=C(p);
@@ -955,7 +955,7 @@
                 eArr.splice(eIndex, 1);
                 break;
               }
-            }
+            //}
           }
         }
       }
@@ -1020,7 +1020,7 @@
         for(let i = 0; i < spokes; ++i){
           if(gunsActive[i] && !retractSpoke[i]){
             //check for squeeze to prevent killing all at once from sideways movement
-            if(squeeze > .98 || squeeze < .02){
+            //if(squeeze > .98 || squeeze < .02){
               p=(playerTheta+(Math.PI*2/spokes*((i+.5)-spokes/2))*squeeze)
               while(p>Math.PI)p-=Math.PI*2
               while(p<-Math.PI)p+=Math.PI*2
@@ -1036,7 +1036,7 @@
                 //playerTheta=e.theta+Math.PI
                 break;
               }
-            }
+            //}
           }
         }
       }
